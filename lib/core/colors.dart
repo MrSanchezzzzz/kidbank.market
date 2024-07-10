f48abc79f42925decea5033065a556e0f65f1fb0
MrSanchezzzzz <dotanrulet@gmail.com>
2024-07-10 14:18:31 +0300

    Colors:refactor

--- lib/core/colors.dart
+++ lib/core/colors.dart
@@ -2,32 +2,32 @@ import 'package:flutter/cupertino.dart';
 
 abstract class Colors{
   //Primary
-  static Color orange500=Color(0xFF6A040F);
-  static Color orange400=Color(0xFFFF7A00);
-  static Color orange300=Color(0xFFFFA34E);
-  static Color orange200=Color(0xFFFFD8B5);
-  static Color orange100=Color(0xFFFFF1E4);
+  static Color orange500=const Color(0xFF6A040F);
+  static Color orange400=const Color(0xFFFF7A00);
+  static Color orange300=const Color(0xFFFFA34E);
+  static Color orange200=const Color(0xFFFFD8B5);
+  static Color orange100=const Color(0xFFFFF1E4);
 
   //Complementary
-  static Color purple700=Color(0xFF451690);
-  static Color purple600=Color(0xFF612FB1);
-  static Color purple500=Color(0xFF766691);
-  static Color purple400=Color(0xFFA884E4);
-  static Color purple300=Color(0xFFD1B7FB);
-  static Color purple200=Color(0xFFDFCBFF);
-  static Color purple100=Color(0xFFF3EDFF);
+  static Color purple700=const Color(0xFF451690);
+  static Color purple600=const Color(0xFF612FB1);
+  static Color purple500=const Color(0xFF766691);
+  static Color purple400=const Color(0xFFA884E4);
+  static Color purple300=const Color(0xFFD1B7FB);
+  static Color purple200=const Color(0xFFDFCBFF);
+  static Color purple100=const Color(0xFFF3EDFF);
 
   //Grey
-  static Color grey500=Color(0xFF212121);
-  static Color grey400=Color(0xFF424242);
-  static Color grey300=Color(0xFF616161);
-  static Color grey200=Color(0xFF9E9E9E);
-  static Color grey100=Color(0xFFE0E0E0);
-  static Color white100=Color(0xFFFFFFFF);
+  static Color grey500=const Color(0xFF212121);
+  static Color grey400=const Color(0xFF424242);
+  static Color grey300=const Color(0xFF616161);
+  static Color grey200=const Color(0xFF9E9E9E);
+  static Color grey100=const Color(0xFFE0E0E0);
+  static Color white100=const Color(0xFFFFFFFF);
 
   //Additional
-  static Color red200=Color(0xFFE30C00);
-  static Color red100=Color(0xFFFFBAB6);
-  static Color green200=Color(0xFF007328);
-  static Color green100=Color(0xFFAAF0BC);
+  static Color red200=const Color(0xFFE30C00);
+  static Color red100=const Color(0xFFFFBAB6);
+  static Color green200=const Color(0xFF007328);
+  static Color green100=const Color(0xFFAAF0BC);
 }
