30f7215f33da7a52abdfc668e979c7f5028d775c
MrSanchezzzzz <dotanrulet@gmail.com>
2024-07-18 15:36:20 +0300

    catalogue: riverpod renamed to display_mode_riverpod

--- lib/features/catalogue/presentation/widgets/view_type_button.dart
+++ lib/features/catalogue/presentation/widgets/view_type_button.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:kidbank/features/catalogue/data/riverpod.dart';
+import 'package:kidbank/features/catalogue/data/display_mode_riverpod.dart';
 
 class ViewTypeButton extends ConsumerStatefulWidget {
   const ViewTypeButton({super.key,this.onTap});
